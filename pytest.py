e6a5ca16f1e310144372b1f053ccaa51027c5de4
Chopinsky <chopinsky@live.com>
2013-12-28 03:00:36 -0500

    Python trials

---
+++ pytest.py
@@ -0,0 +1,15 @@
+'''
+Created on Dec 6, 2013
+
+@author: Ziwei
+'''
+
+if __name__ == '__main__':
+    a = "Hi";
+    b = "World!";
+    print(a + " " + b);
+    myString = raw_input("Enter strings here:");
+    myInt = input("Enter numbers here:")
+    print(myString + ": ");
+    print(myInt);
+    pass
